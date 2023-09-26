f13eaa87f835caf2b122b5023c9d235a2fa47ffa
gentifa576 <rubyling576@yahoo.com>
2023-09-25 23:49:54 -0400

    Separate movement to component

---
+++ Scene/base_component.gd
@@ -0,0 +1,8 @@
+extends Node
+class_name BaseComponent
+
+func component_process(delta):
+	pass
+	
+func component_physics_process(delta):
+	pass
--- Scene/character.gd
+++ Scene/character.gd
@@ -1,50 +1,18 @@
-extends Node2D
+extends CharacterBody2D
 
-@onready var movement_dictionary = {
-	KEY_W: Vector2.UP,
-	KEY_A: Vector2.LEFT,
-	KEY_S: Vector2.DOWN,
-	KEY_D: Vector2.RIGHT
-}
-@onready var left_collision: RayCast2D = $LeftCollision
-@onready var right_collision: RayCast2D = $RightCollision
-@export var velocity = 1
-var direction
-var is_moving = false
+var components = []
 
-# Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	for component in $ComponentContainer.get_children():
+		if component is BaseComponent:
+			components.append(component)
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	if left_collision.is_colliding() || right_collision.is_colliding():
-		is_moving = false
+	for component in components:
+		component.component_process(delta)
 	pass
-	
-func _physics_process(delta):
-	if is_moving:
-		position += direction.normalized() * velocity * delta
 
-func _unhandled_key_input(event):
-	var move_direction = movement_dictionary[event.key_label]
-	if !event.pressed && move_direction:
-		is_moving = false
-		direction = Vector2.ZERO
-#		left_collision.target_position = Vector2.ZERO
-#		right_collision.target_position = Vector2.ZERO
-	
-	if event.pressed && move_direction:
-		direction = movement_dictionary[event.key_label] * 16
-		if move_direction.abs().x > 0:
-			left_collision.target_position = Vector2(move_direction.x * 16, -16)
-			right_collision.target_position = Vector2(move_direction.x * 16, 16)
-		if move_direction.abs().y > 0:
-			left_collision.target_position = Vector2(-16, move_direction.y * 16)
-			right_collision.target_position = Vector2(16, move_direction.y * 16)
-		
-		if left_collision.is_colliding() || right_collision.is_colliding():
-			return
-		else:
-			is_moving = true
+func _physics_process(delta):
+	for component in components:
+		component.component_physics_process(delta)
+	pass
